77c76771d69a5b2eba0c66500e0af81b4f203e94
Georgi Kostov <internal.sun@yahoo.bg>
2017-08-13 17:14:03 +0300

    Extract logic from Spiral::Matrix to Spiral::MatrixRing

--- lib/spiral/matrix.rb
+++ lib/spiral/matrix.rb
@@ -18,38 +18,7 @@ module Spiral
     end
 
     def ring
-      return [] if rows.empty?
-      return rows.first if rows.one?
-
-      north + east + south + west
-    end
-
-    def even?
-      rows.size.even?
-    end
-
-    private
-
-    def north
-      rows[0]
-    end
-
-    def east
-      columns.last[1..-1]
-    end
-
-    def south
-      position = even? ? 0 : 1
-
-      rows.last[position..-2].reverse
-    end
-
-    def west
-      return [] if even? && rows.size == 2
-
-      position = even? && rows.size != 2 ? 1 : 0
-
-      columns.first.reverse[position..-2]
+      MatrixRing.new(self).to_a
     end
   end
 end
--- lib/spiral/matrix_ring.rb
+++ lib/spiral/matrix_ring.rb
@@ -1,4 +1,46 @@
 module Spiral
   class MatrixRing
+    attr_reader :matrix, :rows, :columns
+
+    def initialize(matrix)
+      @matrix  = matrix
+      @rows    = matrix.rows
+      @columns = matrix.columns
+    end
+
+    def to_a
+      return [] if rows.empty?
+      return rows.first if rows.one?
+
+      north + east + south + west
+    end
+
+    def even?
+      rows.size.even?
+    end
+
+    private
+
+    def north
+      rows.first
+    end
+
+    def east
+      columns.last[1..-1]
+    end
+
+    def south
+      position = even? ? 0 : 1
+
+      rows.last[position..-2].reverse
+    end
+
+    def west
+      return [] if even? && rows.size == 2
+
+      position = even? && rows.size != 2 ? 1 : 0
+
+      columns.first.reverse[position..-2]
+    end
   end
 end
